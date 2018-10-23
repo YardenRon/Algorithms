0d72a4bd4e3b4d30a4265716c34ff0eeaf8d64a8
Yarden <yardenron4@gmail.com>
2018-10-23 11:34:41 +0300

    Adding LinkedList implementation

---
+++ Algorithms/Data Structures/LinkedList.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Data_Structures
+{
+    class Node<T> where T: IComparable
+    {
+        public T Value { get; set; }
+        public Node<T> Next { get; set; }
+        public Node<T> Prev { get; set; }
+
+        public Node(T value)
+        {
+            Value = value;
+            Next = null;
+            Prev = null;
+        }
+    }
+
+    class LinkedList<T> where T : IComparable
+    {
+        public Node<T> Head { get; set; }
+
+        public LinkedList()
+        {
+            Head = null;
+        }
+
+        // Run time: 
+        public void insert(T value)
+        {
+            Node<T> newNode = new Node<T>(value);
+            newNode.Next = Head;
+            if (Head != null)
+            {
+                Head.Prev = newNode;
+            }
+
+            Head = newNode;
+        }
+
+        // Run time: 
+        public void delete(T value)
+        {
+            Node<T> current = Head;
+            bool found = false;
+
+            while (current != null || !found)
+            {
+                if (current.Value.CompareTo(value) == 0)
+                {
+                    current.Prev.Next = current.Next;
+                    current.Next.Prev = current.Prev;
+                    found = true;
+                }
+                current = current.Next;
+            }
+        }
+
+        // Run time: 
+        public Node<T> search(T value)
+        {
+            Node<T> current = Head;
+
+            while(current != null)
+            {
+                if (current.Value.CompareTo(value) == 0)
+                {
+                    return current;
+                }
+                current = current.Next;
+            }
+
+            return null;
+        }
+    }
+}
--- Algorithms/Program.cs
+++ Algorithms/Program.cs
@@ -10,16 +10,30 @@ namespace Algorithms
     {
         public static void Main(string[] args)
         {
-            int[] array = { 2, 8, 7, 1, 3, 5, 6, 4 };
+            /* Quick sort */
+            //int[] array = { 2, 8, 7, 1, 3, 5, 6, 4 };
 
-            Console.WriteLine("Array before sorting:");
-            printArray(array);
+            //Console.WriteLine("Array before sorting:");
+            //printArray(array);
 
-            Sorts.QuickSort.execute(array, 0, array.Length-1);
+            //Sorts.QuickSort.execute(array, 0, array.Length-1);
 
-            Console.WriteLine("Array after quick sorting:");
-            printArray(array);
-            Console.ReadLine();
+            //Console.WriteLine("Array after quick sorting:");
+            //printArray(array);
+            //Console.ReadLine();
+            /* Quick sort */
+
+            /* LinkedList */
+            Data_Structures.LinkedList<int> list = new Data_Structures.LinkedList<int>();
+            list.insert(1);
+            list.insert(2);
+            list.insert(5);
+            list.insert(4);
+
+            list.delete(2);
+            var node = list.search(1);
+            /* LinkedList */
+
         }
 
         private static void printArray(int[] array)
