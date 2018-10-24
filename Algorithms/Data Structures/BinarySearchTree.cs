734fe4d7c8ab4d036639e60763e6c0c1672c12c1
Yarden <yardenron4@gmail.com>
2018-10-24 14:25:52 +0300

    Adding Binary Search Tree implementation without the delete procedure yet

---
+++ Algorithms/Data Structures/BinarySearchTree.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Data_Structures
+{
+    class TreeNode<T> where T: IComparable
+    {
+        public T Value { get; set; }
+        public TreeNode<T> Parent { get; set; }
+        public TreeNode<T> Left { get; set; }
+        public TreeNode<T> Right { get; set; }
+
+        public TreeNode(T value)
+        {
+            Value = value;
+            Parent = null;
+            Left = null;
+            Right = null;
+        }
+    }
+
+    class BinarySearchTree<T> where T: IComparable
+    {
+        public TreeNode<T> Root { get; set; }
+
+        public BinarySearchTree()
+        {
+            Root = null;
+        }
+
+        // Run time: O(n)
+        public void inorderTreeWalk(TreeNode<T> root)
+        {
+            if (root != null)
+            {
+                if (root.Left != null)
+                {
+                    inorderTreeWalk(root.Left);
+                }
+
+                Console.WriteLine(root.Value);
+
+                if (root.Right != null)
+                {
+                    inorderTreeWalk(root.Right);
+                }
+            }
+        }
+
+        // Run time: O(n)
+        public TreeNode<T> search(TreeNode<T> root, T value)
+        {
+            if (root != null)
+            {
+                if (root.Value.CompareTo(value) == 0)
+                {
+                    return root;
+                }
+                if (root.Value.CompareTo(value) > 0)
+                {
+                    search(root.Left, value);
+                }
+                else
+                {
+                    search(root.Right, value);
+                }
+            }
+            return null;
+        }
+
+        // Run time: O(h) where h is the height of the tree
+        public void insert(TreeNode<T> node)
+        {
+            TreeNode<T> parent = null;
+            TreeNode<T> current = Root;
+
+            while (current != null)
+            {
+                parent = current;
+                if (node.Value.CompareTo(current.Value) < 0)
+                {
+                    current = current.Left;
+                }
+                else
+                {
+                    current = current.Right;
+                }
+            }
+
+            node.Parent = parent;
+
+            if (parent == null)
+            {
+                Root = node;
+            }
+            else
+            {
+                if (node.Value.CompareTo(parent.Value) < 0)
+                {
+                    parent.Left = node;
+                }
+                else
+                {
+                    parent.Right = node;
+                }
+            }
+        }
+
+        // Run time: O(n)
+        public TreeNode<T> minimum(TreeNode<T> root)
+        {
+            TreeNode<T> current = root;
+            while (current != null)
+            {
+                current = current.Left;
+            }
+            return current;
+        }
+
+        // Run time: O(h) where h is the height of the tree
+        public TreeNode<T> successor(TreeNode<T> node)
+        {
+            if (node != null)
+            {
+                if (node.Right != null)
+                {
+                    return minimum(node.Right);
+                }
+
+                TreeNode<T> parent = node.Parent;
+
+                while (parent != null && node == parent.Right)
+                {
+                    node = parent;
+                    parent = parent.Parent;
+                }
+                return parent;
+            }
+            return null;
+        }
+
+        // Run time: O(h) where h is the height of the tree
+        public void delete(TreeNode<T> node)
+        {
+            if (node != null)
+            {
+                // Case 1: no children
+                if (node.Left == null && node.Right == null)
+                {
+                    if (node.Parent != null)
+                    {
+                        if (node.Parent.Left == node)
+                        {
+                            node.Parent.Left = null;
+                        }
+                        else
+                        {
+                            node.Parent.Right = null;
+                        }
+                    }
+                    else
+                    {
+                        Root = null;
+                    }
+                }
+                else
+                {
+                    // Case 3: 2 children
+                    if (node.Left != null && node.Right != null)
+                    {
+                        // TODO: Complete
+                    }
+                    // Case 2: 1 child
+                    else
+                    {
+                        TreeNode<T> child;
+                        if (node.Left != null)
+                        {
+                            child = node.Left;
+                            node.Left = null;
+                        }
+                        else
+                        {
+                            child = node.Right;
+                            node.Right = null;
+                        }
+                        
+                        if (node.Parent.Left == node)
+                        {
+                            node.Parent.Left = child;
+                        }
+                        else
+                        {
+                            node.Parent.Right = child;
+                        }
+                        node.Parent = null;
+                    }
+                }
+            }
+        }
+    }
+}
--- Algorithms/Program.cs
+++ Algorithms/Program.cs
@@ -24,16 +24,36 @@ namespace Algorithms
             /* Quick sort */
 
             /* LinkedList */
-            Data_Structures.LinkedList<int> list = new Data_Structures.LinkedList<int>();
-            list.insert(1);
-            list.insert(2);
-            list.insert(5);
-            list.insert(4);
+            //Data_Structures.LinkedList<int> list = new Data_Structures.LinkedList<int>();
+            //list.insert(1);
+            //list.insert(2);
+            //list.insert(5);
+            //list.insert(4);
 
-            list.delete(2);
-            var node = list.search(1);
+            //list.delete(2);
+            //var node = list.search(1);
             /* LinkedList */
 
+            /* BinarySearchTree */
+            Data_Structures.BinarySearchTree<int> tree = new Data_Structures.BinarySearchTree<int>();
+            Data_Structures.TreeNode<int> node1 = new Data_Structures.TreeNode<int>(8);
+            Data_Structures.TreeNode<int> node2 = new Data_Structures.TreeNode<int>(9);
+            Data_Structures.TreeNode<int> node3 = new Data_Structures.TreeNode<int>(2);
+            Data_Structures.TreeNode<int> node4 = new Data_Structures.TreeNode<int>(5);
+            Data_Structures.TreeNode<int> node5 = new Data_Structures.TreeNode<int>(4);
+            Data_Structures.TreeNode<int> node6 = new Data_Structures.TreeNode<int>(3);
+            Data_Structures.TreeNode<int> node7 = new Data_Structures.TreeNode<int>(1);
+            tree.insert(node1);
+            tree.insert(node2);
+            tree.insert(node3);
+            tree.insert(node4);
+            tree.insert(node5);
+            tree.insert(node6);
+            tree.insert(node7);
+
+            tree.inorderTreeWalk(tree.Root);
+            Console.ReadLine();
+            /* BinarySearchTree */
         }
 
         private static void printArray(int[] array)
