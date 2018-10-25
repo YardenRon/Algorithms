4afa568ed4c4756879fa4a31e0769c5096692392
Yarden <yardenron4@gmail.com>
2018-10-25 10:47:05 +0300

    Adding stack implementation

---
+++ Algorithms/Data Structures/Stack.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Data_Structures
+{
+    class Stack<T> where T: IComparable
+    {
+        T[] elements;
+        int topIndex;
+
+        public Stack(int size)
+        {
+            elements = new T[size];
+            topIndex = 0;
+        }
+
+        // Run time: O(1)
+        public bool isEmpty()
+        {
+            return topIndex == 0;
+        }
+
+        // Run time: O(1)
+        public T pop()
+        {
+            if (!isEmpty())
+            {
+                topIndex--;
+                return elements[topIndex];
+            }
+            throw new Exception("underflow");
+        }
+
+        // Run time: O(1)
+        public void push(T element)
+        {
+            if (topIndex >= elements.Length)
+            {
+                Array.Resize(ref elements, elements.Length * 2);
+            }
+            elements[topIndex] = element;
+            topIndex++;
+        }
+    }
+}
--- Algorithms/Program.cs
+++ Algorithms/Program.cs
@@ -10,7 +10,8 @@ namespace Algorithms
     {
         public static void Main(string[] args)
         {
-            /* Quick sort */
+
+            #region QuickSort
             //int[] array = { 2, 8, 7, 1, 3, 5, 6, 4 };
 
             //Console.WriteLine("Array before sorting:");
@@ -21,9 +22,9 @@ namespace Algorithms
             //Console.WriteLine("Array after quick sorting:");
             //printArray(array);
             //Console.ReadLine();
-            /* Quick sort */
+            #endregion
 
-            /* LinkedList */
+            #region LinkedList
             //Data_Structures.LinkedList<int> list = new Data_Structures.LinkedList<int>();
             //list.insert(1);
             //list.insert(2);
@@ -32,28 +33,56 @@ namespace Algorithms
 
             //list.delete(2);
             //var node = list.search(1);
-            /* LinkedList */
+            #endregion
 
-            /* BinarySearchTree */
-            Data_Structures.BinarySearchTree<int> tree = new Data_Structures.BinarySearchTree<int>();
-            Data_Structures.TreeNode<int> node1 = new Data_Structures.TreeNode<int>(8);
-            Data_Structures.TreeNode<int> node2 = new Data_Structures.TreeNode<int>(9);
-            Data_Structures.TreeNode<int> node3 = new Data_Structures.TreeNode<int>(2);
-            Data_Structures.TreeNode<int> node4 = new Data_Structures.TreeNode<int>(5);
-            Data_Structures.TreeNode<int> node5 = new Data_Structures.TreeNode<int>(4);
-            Data_Structures.TreeNode<int> node6 = new Data_Structures.TreeNode<int>(3);
-            Data_Structures.TreeNode<int> node7 = new Data_Structures.TreeNode<int>(1);
-            tree.insert(node1);
-            tree.insert(node2);
-            tree.insert(node3);
-            tree.insert(node4);
-            tree.insert(node5);
-            tree.insert(node6);
-            tree.insert(node7);
+            #region BinarySearchTree
+            //Data_Structures.BinarySearchTree<int> tree = new Data_Structures.BinarySearchTree<int>();
+            //Data_Structures.TreeNode<int> node1 = new Data_Structures.TreeNode<int>(8);
+            //Data_Structures.TreeNode<int> node2 = new Data_Structures.TreeNode<int>(9);
+            //Data_Structures.TreeNode<int> node3 = new Data_Structures.TreeNode<int>(2);
+            //Data_Structures.TreeNode<int> node4 = new Data_Structures.TreeNode<int>(5);
+            //Data_Structures.TreeNode<int> node5 = new Data_Structures.TreeNode<int>(4);
+            //Data_Structures.TreeNode<int> node6 = new Data_Structures.TreeNode<int>(3);
+            //Data_Structures.TreeNode<int> node7 = new Data_Structures.TreeNode<int>(1);
+            //tree.insert(node1);
+            //tree.insert(node2);
+            //tree.insert(node3);
+            //tree.insert(node4);
+            //tree.insert(node5);
+            //tree.insert(node6);
+            //tree.insert(node7);
 
-            tree.inorderTreeWalk(tree.Root);
+            //tree.inorderTreeWalk(tree.Root);
+            //Console.ReadLine();
+            #endregion
+
+            #region Stack
+            Data_Structures.Stack<int> stack = new Data_Structures.Stack<int>(5);
+            Console.WriteLine("Stack empty = " + stack.isEmpty());
+            try
+            {
+                stack.pop();
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+            stack.push(1);
+            stack.push(2);
+            stack.push(3);
+            stack.push(4);
+            stack.push(5);
+            stack.push(6);
+            Console.WriteLine("Stack empty = " + stack.isEmpty());
+
+            Console.WriteLine(stack.pop());
+            Console.WriteLine(stack.pop());
+            Console.WriteLine(stack.pop());
+            Console.WriteLine(stack.pop());
+            Console.WriteLine(stack.pop());
+            Console.WriteLine(stack.pop());
             Console.ReadLine();
-            /* BinarySearchTree */
+            #endregion
         }
 
         private static void printArray(int[] array)
